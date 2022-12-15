fc69511dd64bab4e951aacee04466ed33c87daac
nirajvishwakarma <nirajvishwakarma16@gmail.com>
2022-12-15 12:16:00 +0530

    fisrt commit

--- eb-terraform/eb-infrastructure/vpc-backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "eb-tf-infrastructure"
-        key    = "ebullion-infra"
-        region = "ap-south-1"
-    }
-}
--- eb-terraform/web-portal/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-domain_name = "preprod-web.ebullion.in"
-ssl_arn = "arn:aws:acm:us-east-1:590508189671:certificate/3258972b-dbd2-4bb4-9963-027595482907"
